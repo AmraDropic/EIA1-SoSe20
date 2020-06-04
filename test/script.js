5be6752d2cf7d29823cf141ae5783c3ff07b4544
AmraDropic <amra.dropic@hs-furtwangen.de>
2020-06-04 18:45:57 +0200

    neu

--- test/script.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function myFunction() {
-    console.log("asuhpiuashgdiasdhöiad ");
-}
-document.querySelector("h1").addEventListener("click", myFunction);
-//# sourceMappingURL=script.js.map
--- test/script.ts
+++
@@ -1,5 +0,0 @@
-function myFunction() {
-    console.log("asuhpiuashgdiasdhöiad ")
-}
-
-document.querySelector("h1").addEventListener("click", myFunction)
